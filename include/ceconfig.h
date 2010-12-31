af452a1d6aa51d409bf66ed641a3d65b0a883447
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2010-12-31 23:00:35 +0200

    ceconfig.h: Add dummy file.

---
+++ include/ceconfig.h
@@ -0,0 +1,8 @@
+/*
+http://msdn.microsoft.com/en-us/library/ms836805.aspx
+
+CECONFIG.H contains a list of the environment variables for the components
+that were included when a platform was built. This file is automatically
+included in the Windows CE image—the familiar NK.BIN—that is created for
+a platform.
+*/
